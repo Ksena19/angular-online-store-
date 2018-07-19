47e7d510225714fc0ea83c2d82265255f23d7a6d
Oksana <oksanashukh@gmail.com>
2018-07-19 10:33:24 +0300

    changing logo

--- js/app.js
+++ js/app.js
@@ -2,20 +2,31 @@
 angular.module('TransactionApp', [])
 .controller('TransactionsCtrl', function($scope) {
 
-   $scope.title = 'Online-store';
+   $scope.title = 'Add to cart';
 
    $scope.itemsArray = [
-    {  price: 50, name: "Whey protein", img: 'img/item-1.png' },
-    {  price: 60, name: "Protein bar", img: 'img/item-2.png'  },
-    {  price: 35, name: "BCAA", img: 'img/item-3.png'  },
-    {  price: 50, name: "Whey protein", img: 'img/item-1.png'  },
-    {  price: 60, name: "Protein bar", img: 'img/item-2.png'  },
-    {  price: 80, name: "BCAA", img: 'img/item-3.png'  }
+    {  price: 50, name: "Whey protein", img: 'img/item-1.png', quantity: 0},
+    {  price: 60, name: "Protein bar", img: 'img/item-1.png', quantity: 0  },
+    {  price: 35, name: "BCAA", img: 'img/item-1.png', quantity: 0  },
+    {  price: 50, name: "Whey protein", img: 'img/item-1.png', quantity: 0  },
+    {  price: 60, name: "Protein bar", img: 'img/item-1.png', quantity: 0  },
+    {  price: 80, name: "BCAA", img: 'img/item-1.png', quantity: 0  }
 
    ];
-   $scope.count = 0;
-   $scope.addTo = function(item){
-     $scope.count += 1;
+
+   $scope.addTo = function(item) {
+       item.quantity += 1;
+     }
+     $scope.getCost = function(item) {
+      return item.quantity * item.price;
+
+     }
+
+   $scope.cart = [];
+   
+   $scope.getTotal = function() {
+
    }
+   
   
 });
